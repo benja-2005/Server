25c4efb2d24214536e40d0fac9d7c3d2a2bc1582
Alejo Emanuel Alcalde <78708192+mayex-gif@users.noreply.github.com>
2024-05-14 22:45:13 +0000

    Mundo guardado 2024-05-14

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '62021f4b7f96da14cf618494745b3ddba873db29',
+        'reference' => 'c520127ef4ac55cc21fdba2206b1084ec665a8d7',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '62021f4b7f96da14cf618494745b3ddba873db29',
+            'reference' => 'c520127ef4ac55cc21fdba2206b1084ec665a8d7',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
